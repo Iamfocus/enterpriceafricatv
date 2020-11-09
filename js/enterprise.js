94cad67d45295cb9b13a2815f81e044ece7ae675
chrisdesmond <chrisdesmond78@gmail.com>
2020-11-09 08:41:39 +0100

    Enterpriseafricatv

---
+++ js/enterprise.js
@@ -0,0 +1,83 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".scroll").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 18, 2020 00:00:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+
+
+//window.onscroll = function() {scrollFunction()};
+
+//function scrollFunction() {
+  //if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //document.querySelector("nav").style.marginTop = "0";
+ // } else {
+   // document.querySelector("nav").style.marginTop = "140px";
+ // }
+
+//} 
+
+
+$( document ).ready(function() {
+  $( ".tradingview-widget-container iframe" ).clone().appendTo( ".tradingview-widget-container" );
+});
